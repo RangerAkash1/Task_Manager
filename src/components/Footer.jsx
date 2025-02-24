a39a1340f922eb7b45c453c87d67899cf8ec1dbe
RangerAkash <akash13032002@gmail.com>
2025-02-24 15:40:16 +0530

    added about me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import "../css/Footer.css";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <p>© {new Date().getFullYear()} Task Manager. All rights reserved.</p>
+            
             <div className="footer-links">
-                <a href="/about">About</a>
+                <Link to="/about">About</Link>
                 <a href="/privacy">Privacy Policy</a>
                 <a href="https://github.com/RangerAkash1" target="_blank" rel="noopener noreferrer">
                     <FiGithub /> GitHub
